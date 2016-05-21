60bf25a24c1b4b747d1ccfe7139a9a1d8f7cdf78
camscars <camscars702@gmail.com>
2016-05-20 19:39:40 -0700

    project dir

--- landing/settings.py
+++ landing/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-
+PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
 STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
